d8530257af1b62108fb034121155539bbdcf1c56
SJOLK <omarrh0645@gmail.com>
2025-09-11 12:48:49 +0200

    header, footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { Footer } from './components/footer/footer';
+import { Cocktails } from './components/cocktails/cocktails';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, Cocktails],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/cocktails/cocktails.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cocktails',
+  imports: [],
+  templateUrl: './cocktails.html',
+  styleUrl: './cocktails.css'
+})
+export class Cocktails {
+
+}
